2edaf45ea59a14ffa958a06c454e0213fd25f5b9
Suraj Singh <55402839+suraj7086@users.noreply.github.com>
2020-04-12 02:13:19 +0530

    Add files via upload
    Hello World

---
+++ Demonstration_1.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+class Demonstration_1{
+	//This is main class
+	public static void main(String []args){
+		System.out.println("Hello World");//This will print hello world
+	}
+}
