bf660af857e4e924e064d067e01648919f2132db
Suraj Singh <55402839+suraj7086@users.noreply.github.com>
2020-04-23 00:06:49 +0530

    Add files via upload
    Program to find factorial using recursion.

---
+++ Demonstration_12.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+//factorial using recursion.
+class factorial_recursion{
+   
+   public int n;
+   int factorial(int x)
+   {
+   	 if(x ==1 || x==0)
+   	 {
+   	 	return (1);
+   	 }
+   	 else{
+   	 	return (x*factorial(x-1));
+   	 }
+        
+   }
+}
+
+class Demonstration_12{
+	public static void main(String args[]){
+		factorial_recursion f = new factorial_recursion();
+		f.n = Integer.parseInt(args[0]);
+         System.out.println("factorial is : " + f.factorial(f.n));
+
+	}
+}
